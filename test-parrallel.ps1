b0f2659a4d1e7dde4d515fe387cbd2ddc483f1fb
Squazz <GitHub@squazz.dk>
2022-02-11 21:56:51 +0100

    initial commit

---
+++ test-parrallel.ps1
@@ -0,0 +1,60 @@
+# Adapted from / inspired by LawrenceSystems https://forums.lawrencesystems.com/t/5072
+# Bulk DNS Lookup
+#
+# Generates a CSV of DNS lookups from a list of domains.
+#
+# Domain lists
+# - https://oisd.nl/downloads
+# - https://firebog.net/
+# File name/path of domain list:
+
+$domain_list ='coinminers.txt' # One FQDN per line in file.
+
+# IP address of the nameserver used for lookups:
+$ns1_ip='1.1.1.1' # Cloudflare
+$ns2_ip='9.9.9.9' # Quad9
+$ns3_ip='1.1.1.2' # Cloudflare Malware
+$ns4_ip='1.1.1.3' # Cloudflare Malware & Porn
+
+# Start looping through domains
+# foreach($line in Get-Content .\$domain_list) {
+#     $ip1 = Resolve-DnsName -Name $line -Server $ns1_ip -Type A | Select-Object -ExpandProperty IPAddress -Last 1;
+#     $ip2 = Resolve-DnsName -Name $line -Server $ns2_ip -Type A | Select-Object -ExpandProperty IPAddress -Last 1;
+#     $ip3 = Resolve-DnsName -Name $line -Server $ns3_ip -Type A | Select-Object -ExpandProperty IPAddress -Last 1;
+#     $ip4 = Resolve-DnsName -Name $line -Server $ns4_ip -Type A | Select-Object -ExpandProperty IPAddress -Last 1;
+
+#     "$line;$ip1;$ip2;$ip3;$ip4" | add-content -path $file
+# }
+
+#Get the temp path of the context user
+$TempPath = "$PSScriptRoot\TempResult";
+New-Item -Path $TempPath -ItemType Directory # and create a new sub directory
+
+Get-Content .\$domain_list | ForEach-Object -parallel {
+    $ip1 = Resolve-DnsName -Name $_ -Server $ns1_ip -Type A | Select-Object -ExpandProperty IPAddress -Last 1;
+    $ip2 = Resolve-DnsName -Name $_ -Server $ns2_ip -Type A | Select-Object -ExpandProperty IPAddress -Last 1;
+    $ip3 = Resolve-DnsName -Name $_ -Server $ns3_ip -Type A | Select-Object -ExpandProperty IPAddress -Last 1;
+    $ip4 = Resolve-DnsName -Name $_ -Server $ns4_ip -Type A | Select-Object -ExpandProperty IPAddress -Last 1;
+
+    $TempFileName = [System.Guid]::NewGuid().Guid + ".csv" #GUID.txt will ensure randomness
+    $FullTempFilePath = Join-Path -Path "$using:TempPath" -ChildPath $TempFileName
+    "$line;$ip1;$ip2;$ip3;$ip4" | Out-File -Path $FullTempFilePath -Force #Write out to the random file
+}
+
+# Start CSV
+$fileName = 'result.csv';
+$file = "$PSScriptRoot\$fileName";
+
+New-Item -Path $PSScriptRoot -Name $fileName -ItemType "file" -Force
+
+"Domain name;$ns1_ip;$ns2_ip;$ns3_ip;$ns4_ip" | add-content -path $file
+
+$TempFiles = Get-ChildItem -Path $TempPath
+foreach ($TempFile in $TempFiles) {
+    Get-Content -Path $TempFile.FullName | Out-File $file -Append #concatenate all the files
+}
+
+Remove-Item -Path $TempPath -Force -Recurse #clean up
+
+# In Excel
+# =COUNTV(B3:B5000)-COUNTIF(B3:B5000;"0.0.0.0")
---
+++ test.ps1
@@ -0,0 +1,38 @@
+# Adapted from / inspired by LawrenceSystems https://forums.lawrencesystems.com/t/5072
+# Bulk DNS Lookup
+#
+# Generates a CSV of DNS lookups from a list of domains.
+#
+# Domain lists
+# - https://oisd.nl/downloads
+# - https://firebog.net/
+# File name/path of domain list:
+
+$domain_list ='coinminers.txt' # One FQDN per line in file.
+
+# IP address of the nameserver used for lookups:
+$ns1_ip='1.1.1.1' # Cloudflare
+$ns2_ip='9.9.9.9' # Quad9
+$ns3_ip='1.1.1.3' # Cloudflare Malware & Porn
+$ns4_ip='10.0.0.123' # Own DNS
+
+# Start CSV
+$fileName = 'result.csv';
+$file = "$PSScriptRoot\$fileName";
+
+New-Item -Path $PSScriptRoot -Name $fileName -ItemType "file" -Force
+
+"Domain name;$ns1_ip;$ns2_ip;$ns3_ip;$ns4_ip" | add-content -path $file
+
+# Start looping through domains
+foreach($line in Get-Content .\$domain_list) {
+    $ip1 = Resolve-DnsName -Name $line -Server $ns1_ip -Type A | Select-Object -ExpandProperty IPAddress -Last 1;
+    $ip2 = Resolve-DnsName -Name $line -Server $ns2_ip -Type A | Select-Object -ExpandProperty IPAddress -Last 1;
+    $ip3 = Resolve-DnsName -Name $line -Server $ns3_ip -Type A | Select-Object -ExpandProperty IPAddress -Last 1;
+    $ip4 = Resolve-DnsName -Name $line -Server $ns4_ip -Type A | Select-Object -ExpandProperty IPAddress -Last 1;
+
+    "$line;$ip1;$ip2;$ip3;$ip4" | add-content -path $file
+}
+
+# In Excel
+# =COUNTV(B3:B5000)-COUNTIF(B3:B5000;"0.0.0.0")
